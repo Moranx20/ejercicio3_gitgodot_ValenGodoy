6fd8b1875229b7f49fd7e0e5219f7e78d795b447
Valen_G <valuchi068@gmail.com>
2026-04-13 00:56:15 -0300

    doblesalto_dash

--- escenas/player.gd
+++ escenas/player.gd
@@ -1,14 +1,44 @@
 extends CharacterBody2D
 
 var move_speed = 300
+var dash_speed = 800
+var dashing= false
 var jump_speed = 600
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var doublejump: bool = false
 
 func _physics_process(_delta):
+	if not is_on_floor():
+		velocity.y += gravity * _delta
+	else:
+		doublejump = false
+	
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_speed
-	velocity.y += gravity * _delta
+		if is_on_floor():
+			velocity.y = -jump_speed
+			doublejump = true 
+		else:
+			if doublejump:  
+				velocity.y = -jump_speed
+				doublejump = false
+	else:
+		if Input.is_action_just_released("jump"):
+			velocity.y += 200
+	
+	if Input.is_action_just_pressed("dash"):
+		dashing = true
+		$Dash_timer.start()
 	
 	var input_axis = Input.get_axis("move_left","move_right")
 	velocity.x = input_axis * move_speed
+	
+	if input_axis:
+		if dashing:
+			velocity.x = input_axis * dash_speed
+		else:
+			velocity.x = input_axis * move_speed
 	move_and_slide()
+
+
+func _on_dash_timer_timeout():
+	dashing = false
