c551776a3162d5e42d7d2ae0dadffaf3f62a5496
Valen_G <valuchi068@gmail.com>
2026-04-19 20:06:32 -0300

    sprite_caballerito

--- escenas/player.gd
+++ escenas/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-var move_speed = 300
-var dash_speed = 800
+var move_speed = 200
+var dash_speed = 500
 var dashing= false
 var jump_speed = 600
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var doublejump: bool = false
+@onready var animationPlayer = $AnimationPlayer
+@onready var sprite2D = $Sprite2D
 
 func _physics_process(_delta):
 	if not is_on_floor():
@@ -27,10 +29,16 @@ func _physics_process(_delta):
 	
 	if Input.is_action_just_pressed("dash"):
 		dashing = true
+		animationPlayer.play("dash")
 		$Dash_timer.start()
 	
 	var input_axis = Input.get_axis("move_left","move_right")
 	velocity.x = input_axis * move_speed
+	
+	if input_axis == 1:
+		sprite2D.flip_h = false
+	elif input_axis == -1:
+		sprite2D.flip_h = true
 	
 	if input_axis:
 		if dashing:
@@ -38,6 +46,19 @@ func _physics_process(_delta):
 		else:
 			velocity.x = input_axis * move_speed
 	move_and_slide()
+	animations(input_axis)
+	
+func animations(input_axis):
+	if is_on_floor():
+		if input_axis == 0:
+			animationPlayer.play("idle")
+		else:
+			animationPlayer.play("caminar")
+	else:
+		if velocity.y < 0:
+			animationPlayer.play("jump")
+		elif velocity.y > 0:
+			animationPlayer.play("caida")
 
 
 func _on_dash_timer_timeout():
